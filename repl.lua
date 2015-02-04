2b0b9877dd853fffb192c2255a50345762a0acf8
Tim Caswell <tim.caswell@rackspace.com>
2015-02-03 22:53:36 -0600

    Finish https request code

--- repl.lua
+++ repl.lua
@@ -3,6 +3,7 @@ local connect = require('creationix/coro-tcp').connect
 local tlsWrap = require('creationix/coro-tls').wrap
 local httpCodec = require('creationix/http-codec')
 local wrapper = require('creationix/coro-wrapper')
+local jsonParse = require('creationix/json').parse
 
 local deviceID = env.get("SPARK_DEVICE_ID")
 local accessToken = env.get("SPARK_ACCESS_TOKEN")
@@ -14,6 +15,13 @@ if not accessToken then
   error("SPARK_ACCESS_TOKEN require in environment")
 end
 
+local req = {
+  method = "GET",
+  path = "/v1/devices/" .. deviceID .. "/endpoint?access_token=" .. accessToken,
+  {"Host", "api.spark.io"},
+  {"User-Agent", "lit"},
+  {"Accept", "*/*"},
+}
 local read, write = assert(connect("api.spark.io", "https"))
 read, write = tlsWrap(read, write)
 
@@ -21,12 +29,15 @@ read = wrapper.reader(read, httpCodec.decoder())
 write = wrapper.writer(write, httpCodec.encoder())
 
 
-local req = {
-  method = "GET",
-  path = "/v1/devices/" .. deviceID .. "/endpoint?access_token=" .. accessToken
-}
+p(req)
 write(req)
 local res = read()
-
-p(req)
 p(res)
+local body = {}
+for chunk in read do
+  body[#body + 1] = chunk
+  if #chunk == 0 then break end
+end
+body = table.concat(body)
+p(jsonParse(body))
+write()
